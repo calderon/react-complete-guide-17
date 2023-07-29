36c19e519d24ea69a1b1011130bee35866a18fb3
Daniel Calderón Portillo <daniel.calderon.portillo@gmail.com>
2023-07-29 12:30:03 +0200

    Task 5

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import * as EmailValidator from 'email-validator';
 
 const SimpleInput = (props) => {
   const [name, setName] = useState('');
   const [isNameChanged, setNameChanged] = useState(false);
+  const [email, setEmail] = useState('');
+  const [isEmailChanged, setEmailChanged] = useState(false);
 
   const nameIsValid = name.trim() !== '';
   const isNameInvalid = !nameIsValid && isNameChanged;
+  const emailIsValid = EmailValidator.validate(email)
+  const isEmailInvalid = !emailIsValid && isEmailChanged;
 
-  const isFormValid = nameIsValid;
+  const isFormValid = nameIsValid && emailIsValid;
 
   const nameChangeHandler = (evt) => {
     setName(evt.target.value);
@@ -17,40 +22,65 @@ const SimpleInput = (props) => {
     setNameChanged(true);
   };
 
+  const emailChangeHandler = (evt) => {
+    setEmail(evt.target.value);
+  };
+
+  const emailBlurHandler = (evt) => {
+    setEmailChanged(true);
+  };
+
   const formSubmitHandler = (evt) => {
     evt.preventDefault();
     setNameChanged(true);
+    setEmailChanged(true);
 
     if (!isFormValid) {
       return;
     }
-
-    console.log(name);
-    setName('');
   };
 
-  const formClass = () => {
+  const nameFormClass = () => {
     const classes = ['form-control'];
 
-    if (!isFormValid && isNameChanged) {
+    if (isNameInvalid) {
       classes.push('invalid')
     }
 
     return classes.join(' ');
-  }
+  };
+
+  const emailFormClass = () => {
+    const classes = ['form-control'];
+
+    if (isEmailInvalid) {
+      classes.push('invalid')
+    }
+
+    return classes.join(' ');
+  };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={formClass()}>
+      <div className={nameFormClass()}>
         <label htmlFor='name'>Your Name</label>
         <input id='name'
                type='text'
                onChange={nameChangeHandler}
                onBlur={nameBlurHandler}
                value={name} />
+        {isNameInvalid && <p style={{ 'color': 'red'}}>Not valid</p>}
       </div>
 
-      {isNameInvalid && <p style={{ 'color': 'red'}}>Not valid</p>}
+      <div className={emailFormClass()}>
+        <label htmlFor='email'>Your E-mail</label>
+        <input id='email'
+               type='email'
+               onChange={emailChangeHandler}
+               onBlur={emailBlurHandler}
+               value={email} />
+        {isEmailInvalid && <p style={{ 'color': 'red'}}>Not valid</p>}
+      </div>
 
       <div className="form-actions">
         <button disabled={!isFormValid}>Submit</button>
